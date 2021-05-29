42b3182e6d6501f35c5c81cea3d5c4a3a452e7b3
Balytsky Igor <ibalyts@live.com>
2021-05-29 18:50:09 +0300

    IA consult 29-05

--- WinFormsUI/Form1.Designer.cs
+++ WinFormsUI/Form1.Designer.cs
@@ -28,20 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.splitter1 = new System.Windows.Forms.Splitter();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.lInfo = new System.Windows.Forms.Label();
+            this.bMove = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbName
             // 
-            this.textBox1.Location = new System.Drawing.Point(280, 205);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.tbName.Location = new System.Drawing.Point(376, 265);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(132, 22);
+            this.tbName.TabIndex = 1;
             // 
             // label1
             // 
@@ -49,17 +55,19 @@
             this.label1.BackColor = System.Drawing.SystemColors.Menu;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.CausesValidation = false;
-            this.label1.Location = new System.Drawing.Point(386, 208);
+            this.label1.Location = new System.Drawing.Point(364, 242);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.Size = new System.Drawing.Size(115, 19);
             this.label1.TabIndex = 2;
             this.label1.Text = "Enter your name";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(334, 262);
+            this.button2.Location = new System.Drawing.Point(375, 295);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 30);
+            this.button2.Size = new System.Drawing.Size(133, 37);
             this.button2.TabIndex = 3;
             this.button2.Text = "Start!";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,9 +75,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(346, 231);
+            this.button1.Location = new System.Drawing.Point(686, 295);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.Size = new System.Drawing.Size(100, 31);
             this.button1.TabIndex = 4;
             this.button1.Text = "Join game";
             this.button1.UseVisualStyleBackColor = true;
@@ -83,18 +92,54 @@
             this.splitter1.TabIndex = 0;
             this.splitter1.TabStop = false;
             // 
+            // tbCode
+            // 
+            this.tbCode.Location = new System.Drawing.Point(677, 265);
+            this.tbCode.Margin = new System.Windows.Forms.Padding(4);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(132, 22);
+            this.tbCode.TabIndex = 5;
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Interval = 5000;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // lInfo
+            // 
+            this.lInfo.AutoSize = true;
+            this.lInfo.Location = new System.Drawing.Point(371, 58);
+            this.lInfo.Name = "lInfo";
+            this.lInfo.Size = new System.Drawing.Size(46, 17);
+            this.lInfo.TabIndex = 6;
+            this.lInfo.Text = "label2";
+            // 
+            // bMove
+            // 
+            this.bMove.Location = new System.Drawing.Point(845, 106);
+            this.bMove.Name = "bMove";
+            this.bMove.Size = new System.Drawing.Size(75, 23);
+            this.bMove.TabIndex = 7;
+            this.bMove.Text = "button3";
+            this.bMove.UseVisualStyleBackColor = true;
+            this.bMove.Click += new System.EventHandler(this.bMove_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.bMove);
+            this.Controls.Add(this.lInfo);
+            this.Controls.Add(this.tbCode);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbName);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "WARSHIP";
             this.ResumeLayout(false);
@@ -103,11 +148,15 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Splitter splitter1;
         public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Label lInfo;
+        private System.Windows.Forms.Button bMove;
     }
 }
 
--- WinFormsUI/Form1.cs
+++ WinFormsUI/Form1.cs
@@ -14,15 +14,89 @@ namespace WinFormsUI
 {
     public partial class Form1 : Form
     {
-        int joinOrStart=-1;
+        private ISeaBattle game;
+        int numOfPlayer;
         string name;
         Label ulljoingame;
+        string gameCode;
+        private IFieldReader fieldReader = new FileFieldReader("field.txt");
+        Label[,] myField = new Label[10, 10];
+        Label[,] anamiesField = new Label[10, 10];
+        List<Label[,]> fields;
+        Panel pnlMy = new Panel();
+        Panel pnlEnemy = new Panel();
+        Panel[] panels;
+        string code;
         public Form1()
         {
             InitializeComponent();
+            Connect();
+            CreateFields();
+            game = new Game();
+        }
+
+        private void CreateFields()
+        {
+            panels = new Panel[] { pnlMy, pnlEnemy };
+            fields = new List<Label[,]>() { myField, anamiesField };
+            for (int i = 0; i < 2; i++)
+            {
+                //panels[i].Location = new Point( i* 10*20)
+                Label l = new Label();
+                l.Click += L_Click;
+                panels[i].Controls.Add(l);
+
+                Controls.Add(panels[i]);
+            }
+        }
+
+        private void L_Click(object sender, EventArgs e)
+        {
+            Label l = sender as Label;
+
+            for (int i = 0; i < anamiesField.GetLength(0); i++)
+            {
+                for (int j = 0; j < anamiesField.GetLength(1); j++)
+                {
+                    if(l == anamiesField[i, j])
+                    {
+                        code = GetCode(i, j);
+                        return;
+                    }
+                }
+            }
+            //найти номер лабел, записать в переменную ход. code (char)((int)('a') + i) 
+
+        }
+
+        private string GetCode(int i, int j)
+        {
+            return $"{(char)('a' + i)}{j}";
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+
+            gameCode = game.StartGame();
+
+            JoinGame();
+
+        }
+
+        private void JoinGame()
+        {
+            label1.Hide();
+            button1.Hide();
+            button2.Hide();
+
+            name = tbName.Text;
+            tbName.Hide();
+
+            numOfPlayer = game.JoinGame(gameCode, name, fieldReader.ReadField());
+            mainTimer.Start();
+        }
+
+        private static void Connect()
         {
             Uri tcpUri = new Uri("http://localhost:8000/SeaBattleServer");
             // Создаём сетевой адрес, с которым клиент будет взаимодействовать
@@ -32,37 +106,61 @@ namespace WinFormsUI
             ChannelFactory<ISeaBattle> factory = new ChannelFactory<ISeaBattle>(binding, address);
             // Открываем канал для общения клиента с со службой
             ISeaBattle service = factory.CreateChannel();
-            label1.Dispose();
-            button1.Dispose();
-            button2.Dispose();
-            if (joinOrStart == -1)
-            {
-                name = textBox1.Text;
-                textBox1.Dispose();
-                SeaBattleWFUI ui = new SeaBattleWFUI(new Game(), name);
-                ui.Start();
-            }
-            else
-            {
-                int gameCode = Convert.ToInt32(textBox1.Text);
-                textBox1.Dispose();
-                ulljoingame.Dispose();
-                SeaBattleWFUI ui = new SeaBattleWFUI(new Game(), name, gameCode.ToString());
-                ui.Start();
-            }
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            joinOrStart++;
-            ulljoingame = new Label();
-            ulljoingame.Text = "You will join someone's game";
-            ulljoingame.Location = button1.Location;
-            button1.Dispose();
-            label1.Text = "Enter game code";
-            name = textBox1.Text;
-            this.Controls.Add(ulljoingame);
+            gameCode = tbCode.Text;
+            JoinGame();
         }
 
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            string res;
+            res = game.Update(gameCode, numOfPlayer);
+            lInfo.Text = res;
+            if (res != "Ready")
+            {
+                bMove.Enabled = false;
+            }
+
+            if (res == "Ready" && game.CheckWinner(gameCode) == -1)
+            {
+                PrintBoard(game.GetBoard(gameCode, numOfPlayer));
+                bMove.Enabled = true;
+                lInfo.Text = "Your move";
+            }
+            if (game.CheckWinner(gameCode) != -1)
+            {
+                if (game.CheckWinner(gameCode) == numOfPlayer)
+                {
+                    lInfo.Text = "You have won!!!";
+                    bMove.Enabled = false;
+                }
+                else
+                {
+                    lInfo.Text = "You r looser";
+                    bMove.Enabled = false;
+                }
+                mainTimer.Stop();
+            }
+        }
+
+        private void PrintBoard(string[] field)
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    myField[j, i].Text = field[i][j].ToString();
+                }
+            }
+        }
+
+        private void bMove_Click(object sender, EventArgs e)
+        {
+            game.Shot(gameCode, numOfPlayer, code);
+        }
     }
 }
